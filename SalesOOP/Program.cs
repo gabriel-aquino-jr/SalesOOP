ee75f4350aeec31127c689d09edaad64c7d6af34
galzer <galzer@outlook.com>
2023-01-15 22:30:30 -0400

    Add project files.

---
+++ SalesOOP/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * AUTHOR: Gabriel Aquino
+ * DATE: 01/15/2023
+ * PURPOSE: Simple OOP Sales Entities
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer(1, "1111.2222.3333.4444");
+
+            Product p1 = new Product(1, "Milk", 3.99m);
+            Product p2 = new Product(2, "Coffee", 7.99m);
+            Product p3 = new Product(3, "Butter", 4.99m);
+            Product p4 = new Product(4, "Bun", 6.99m);
+
+            OrderItem oi1 = new OrderItem(1, p1, 3);
+            OrderItem oi2 = new OrderItem(2, p2, 1);
+
+            List<OrderItem> l1 = new List<OrderItem>();
+            l1.Add(oi1);
+            l1.Add(oi2);
+
+            Order o1 = new Order(1, c1, new DateTime(2022, 01, 15, 20, 12, 0, 0), l1);
+
+            Console.WriteLine(o1.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SalesOOP/Sales.cs
@@ -0,0 +1,126 @@
+﻿/*
+ * AUTHOR: Gabriel Aquino
+ * DATE: 01/15/2023
+ * PURPOSE: Simple OOP Sales Entities
+ */
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesOOP
+{
+    public class Customer
+    {
+        public Customer(int customerId, string cardNumber)
+        {
+            CustomerId = customerId;
+            CardNumber = cardNumber;
+        }
+
+        public int CustomerId { get; set; }
+        public string CardNumber { get; set; }        
+    }
+
+    public class Order
+    {
+        public Order(int orderId, Customer customer, DateTime datePurchase, List<OrderItem> items)
+        {
+            OrderId = orderId;
+            Customer = customer;
+            DatePurchase = datePurchase;
+            Items = items;
+        }
+
+        public int OrderId { get; set;}
+
+        public Customer Customer { get; set; }
+
+        public DateTime DatePurchase { get; set; }
+
+        public List<OrderItem> Items { get; set; }
+
+        public decimal OrderTotal
+        {
+            get
+            {                
+                return Items.Sum(i => i.TotalItemAmount);
+            }
+        }
+
+        public double TotalItems
+        {
+            get
+            {
+                return Items.Sum(i => i.Quantity);
+            }
+        }
+
+        public override string ToString()
+        {
+            string lineBar = "-----------------------------------------------------------";
+            string header = $"\t\t\tSales Store\r\n";
+            header += $"\t\tDate { this.DatePurchase}\r\n";
+            header += $"{lineBar}\r\n";
+            header += $"#\tItem\t\tQty\tUnit Price\tTotalPrice\r\n";
+            string details = "";
+            foreach (OrderItem item in Items)
+            {
+                details += $"{item.ItemNum}\t{item.Product.ProductName}\t\t{item.Quantity}\t{item.Product.Price:c2}\t\t{item.TotalItemAmount:c2}\r\n";
+            }
+            string footer = $"{lineBar}\r\n";
+            footer += $"{this.TotalItems} Items\t\t\t\tOrder Total:\t{this.OrderTotal:c2}\r\n";
+            footer += $"{lineBar}\r\n";
+            footer += "TYPE:  PURCHASE\r\n";
+            footer += $"ACCT:  FLASH DEFAULT\t\t\tCAD$ {this.OrderTotal:c2}\r\n";
+            footer += "Card Type:  DEBIT\r\n";
+            string fullCardNumber = this.Customer.CardNumber;
+            footer += $"CARD NUMBER:\t ****.****.****{fullCardNumber.Substring(fullCardNumber.LastIndexOf("."))} ";
+
+
+
+            return header + details + footer;
+        }
+    }
+
+    public class OrderItem
+    {
+        public OrderItem(int itemNum, Product product, double quantity)
+        {
+            ItemNum = itemNum;
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public int ItemNum { get; set; }
+        public Product Product { get; set; }
+        public double Quantity { get; set; }
+
+        public decimal TotalItemAmount {
+            get
+            {
+                return Product.Price * Convert.ToDecimal( Quantity );
+            }
+        }
+    }
+
+    public class Product
+    {
+        public Product(int productId, string productName, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+        }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
